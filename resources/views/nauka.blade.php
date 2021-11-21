b54c79164cddb838154e3f0251c2489e0b4fa590
Gac Dariusz <darekgac1998@gmail.com>
2021-11-21 21:34:09 +0100

    Initial commit

---
+++ resources/views/nauka.blade.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+
+        body {
+            background: #1a202d;
+        }
+        .main {
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .tresc {
+            width: 33.33333%;
+            height: auto;
+            margin-bottom: 50px;
+        }
+        .subTresc {
+            background: #4a5568;
+            width: 50%;
+            height: 50%;
+            margin: auto;
+        }
+        .subSubTresc {
+            background: #374b6d;
+            padding: 20px 30px;
+            color: #3cac3f;
+            text-align: center;
+            min-height: 25vh;
+            width: 100%;
+        }
+
+    </style>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+
+
+    <div class="main">
+
+
+        <div class="tresc">
+            <div class="subTresc">
+
+                <div class="subSubTresc">
+                    <span class="material-icons">face</span>
+                    <p>dsad dads a dsa</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="tresc">
+            <div class="subTresc">
+
+                <div class="subSubTresc">
+                    <span class="material-icons">face</span>
+                    <p>Twarzyczki Testowe</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="tresc">
+            <div class="subTresc">
+
+                <div class="subSubTresc">
+                    <span class="material-icons">face</span>
+                    <p>dsad ad sada dastowe</p>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="tresc">
+            <div class="subTresc">
+
+                <div class="subSubTresc">
+                    <span class="material-icons">face</span>
+                    <p>adsd adsa dsa</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="tresc">
+            <div class="subTresc">
+
+                <div class="subSubTresc">
+                    <span class="material-icons">face</span>
+                    <p>dsa dasd asdsa</p>
+                </div>
+
+            </div>
+        </div>
+
+{{--        <div class="tresc">--}}
+{{--            <div class="subTresc">--}}
+
+{{--                <div class="subSubTresc">--}}
+{{--                    6--}}
+{{--                </div>--}}
+
+{{--            </div>--}}
+{{--        </div>--}}
+
+
+    </div>
+
+
+</body>
+</html>
